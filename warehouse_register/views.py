94c60f6f075319da286f15887087e3a2ad67ca4e
Mohammed <hgyfhx33@gmail.com>
2024-03-02 15:07:32 +0400

    https://github.com/almr7li/warehouse

--- warehouse_register/views.py
+++ warehouse_register/views.py
@@ -1,11 +1,20 @@
 # views.py
-
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Goods
 from .forms import GoodsForm
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+from rest_framework.decorators import api_view
 
+
+ID_PARAM = openapi. Parameter('id', openapi. IN_QUERY, description="id", type=openapi.TYPE_INTEGER)
+
+
+# 'method' can be used to customize a single HTTP method of a view
+@swagger_auto_schema(method='get', manual_parameters=[ID_PARAM])
+@api_view(['GET'])
 def goods_form(request, id=0):
     if request.method == "GET":
         if id == 0:
@@ -48,16 +57,3 @@ class CustomLoginView(LoginView):
     redirect_authenticated_user = True
     success_url = reverse_lazy('goods_list')
 
-# views.py
-
-from django.shortcuts import render
-from .models import Goods
-
-def goods_list(request):
-    search_query = request.GET.get('search', '')
-    goods_list = Goods.objects.all()
-    if search_query:
-        goods_list = goods_list.filter(name__icontains=search_query) | \
-                     goods_list.filter(price__icontains=search_query) | \
-                     goods_list.filter(quantity__icontains=search_query)
-    return render(request, "warehouse_register/goods_list.html", {'goods_list': goods_list, 'search_query': search_query})
